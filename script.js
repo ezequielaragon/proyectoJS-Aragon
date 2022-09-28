4fa25b3febc8a4f803abe06286bad491a68e6ba9
ezearagon <magiceze@gmail.com>
2022-09-27 22:36:46 -0300

    Tienda de productos

--- script.js
+++ script.js
@@ -1,182 +1,188 @@
-const carritoItems = [];
-const carritoTotal = [];
-
-class Producto{
-    constructor(categoria, nombre, precio){
-        this.categoria = categoria;
-        this.nombre = nombre;
-        this.precio = parseFloat(precio);
+const productos = [
+    {
+        id: 1,
+        categoria: "Combos PS5",
+        nombre: "PlayStation 5 Standart 825gb + 2 Joysticks + Juego físico de regalo",
+        precio: 300000,
+        img: "./assets/img/ps5-2joy-1juego.jpeg",
+        cantidad: 1
+    },
+    {
+        id: 2,
+        categoria: "Combos PS5",
+        nombre: "PlayStation 5 Digital 825gb + Joystick",
+        precio: 280000,
+        img: "./assets/img/ps5-1joy.jpg",
+        cantidad: 1
+    },
+    {
+        id: 3,
+        categoria: "Combos PS5",
+        nombre: "PlayStation 5 Digital 825gb",
+        precio: 270000,
+        img: "./assets/img/ps5-digital.JPG",
+        cantidad: 1
+    },
+    {
+        id: 4,
+        categoria: "Accesorios",
+        nombre: "Joystick DualSense (color a elección)",
+        precio: 15000,
+        img: "./assets/img/1joy.JPG",
+        cantidad: 1
+    },
+    {
+        id: 5,
+        categoria: "Accesorios",
+        nombre: "2 Joystick DualSense (color a elección)",
+        precio: 28000,
+        img: "./assets/img/2joy.png",
+        cantidad: 1
+    },
+    {
+        id: 6,
+        categoria: "Accesorios",
+        nombre: "Auriculares PS5 Sony Pulse 3D",
+        precio: 60000,
+        img: "./assets/img/auris.jpeg",
+        cantidad: 1
     }
-}
+];
 
-const producto1 = new Producto('Combos PS5', 'PlayStation 5 Standart 825gb + 2 Joysticks + Juego físico de regalo', 300000)
-const producto2 = new Producto('Combos PS5', 'PlayStation 5 Digital 825gb + Joystick', 280000)
-const producto3 = new Producto('Combos PS5', 'PlayStation 5 Digital 825gb', 270000)
-const producto4 = new Producto('Accesorios', 'Joystick DualSense (color a elección)', 15000)
-const producto5 = new Producto('Accesorios', '2 Joystick DualSense (color a elección)', 28000)
-const producto6 = new Producto('Accesorios', 'Auriculares PS5 Sony Pulse 3D', 60000)
+let carrito = JSON.parse(localStorage.getItem("carrito"));
 
-function seleccionarConsola(){
-    function opcionesConsolas (){
-        let combosConsolas = prompt(
-            `Elija la opción deseada
-            \n 1- ${producto1.nombre}, $${producto1.precio}
-            \n 2- ${producto2.nombre}, $${producto2.precio}
-            \n 3- ${producto3.nombre}, $${producto3.precio}
-            \nPresioná X para salir.`
-        )
-        return combosConsolas;
-    }
+function renderizarProductos(){
+    
+    const tienda = document.getElementById('tienda');  
 
-    combosConsolas = opcionesConsolas();
-    while (combosConsolas != 'X' && combosConsolas != 'x'){
+    productos.forEach((p) => {
+        let producto = document.createElement('div');
+        producto.classList.add('col-12');
+        producto.classList.add('col-md-4');
+        producto.classList.add('mb-5');
+        producto.classList.add('d-flex');
+        producto.classList.add('justify-content-center');
+
+        producto.innerHTML = `
+        <div class="card text-dark" style="width: 18rem;">
+            <img class="card-img-top" src="${p.img}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${p.nombre}</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p>$${p.precio}</p>
+                <button class="btn btn-primary" id="${p.id}">Añadir al carrito</button>
+            </div>
+        </div>
+        `
+
+        tienda.appendChild(producto);
+
+        producto.querySelector('button').addEventListener('click', ()=>{
+            
+            agregarProductosAlCarrito(p.id);
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+        })
         
-        let comboSeleccionado = null;
+    })
+}
+
+renderizarProductos();
+
+function agregarProductosAlCarrito(id){
+    
+    let producto = productos.find(producto => producto.id === id);
+
+    let productoEnCarrito = carrito.find(producto => producto.id === id);
+
+    if(productoEnCarrito){
         
-        switch(combosConsolas){
-            case '1':
-                comboSeleccionado = producto1;
-                carritoItems.push(producto1.nombre);
-                carritoTotal.push(producto1.precio);
-                alert(`Se ha agregado "${producto1.nombre}" a su carrito.`);
-                break;
-            case '2':
-                comboSeleccionado = producto2;
-                carritoItems.push(producto2.nombre);
-                carritoTotal.push(producto2.precio);
-                alert(`Se ha agregado "${producto2.nombre}" a su carrito.`);
-                break;
-            case '3':
-                comboSeleccionado = producto3;
-                carritoItems.push(producto3.nombre);
-                carritoTotal.push(producto3.precio);
-                alert(`Se ha agregado "${producto3.nombre}" a su carrito.`);
-                break;
-            default:
-                alert('Opción inválida')
-                break;
-        }
-        combosConsolas = opcionesConsolas();
-    }
-}
+        productoEnCarrito.cantidad++;
 
-function seleccionarAccesorios(){
-    function opcionesAccesorios(){
-        let accesoriosConsola = prompt(
-            `Elija la opción deseada
-            \n 1- ${producto4.nombre}, $${producto4.precio}
-            \n 2- ${producto5.nombre}, $${producto5.precio}
-            \n 3- ${producto6.nombre}, $${producto6.precio}
-            \nPresioná X para salir.`
-        )
-        return accesoriosConsola;
-    }
-    accesoriosConsola = opcionesAccesorios();
-    while (accesoriosConsola != 'X' && accesoriosConsola != 'x'){
+        console.log(carrito);
+
+        alert(`La cantidad del producto ${producto.nombre} fue modificada`);
+
+    }else {
         
-        let comboSeleccionado = null;
+        producto.cantidad = 1;
+
+        carrito.push(producto);
+
+        console.log(carrito);
+
+        alert('Producto agregado correctamente al carrito')
+    }
+    renderizarCarrito();
+    calcularTotal();
+}
+
+function renderizarCarrito(){
+
+    const d = document;
+    let carritoHTML = d.querySelector('#carrito');
+
+    carritoHTML.innerHTML = '';
+
+    carrito.forEach((p, index)=> {
+    
+        let producto = document.createElement('div');
+        producto.classList.add('col-12');
+        producto.classList.add('col-md-4');
+        producto.classList.add('mb-5');
+        producto.classList.add('d-flex');
+        producto.classList.add('justify-content-center');
+
+        producto.innerHTML = `
         
-        switch(accesoriosConsola){
-            case '1':
-                comboSeleccionado = producto4;
-                carritoItems.push(producto4.nombre);
-                carritoTotal.push(producto4.precio);
-                alert(`Se ha agregado "${producto4.nombre}" a su carrito.`);
-                break;
-            case '2':
-                comboSeleccionado = producto5;
-                carritoItems.push(producto5.nombre);
-                carritoTotal.push(producto5.precio);
-                alert(`Se ha agregado "${producto5.nombre}" a su carrito.`);
-                break;
-            case '3':
-                comboSeleccionado = producto6;
-                carritoItems.push(producto6.nombre);
-                carritoTotal.push(producto6.precio);
-                alert(`Se ha agregado "${producto6.nombre}" a su carrito.`);
-                break;
-            default:
-                alert('Opción inválida')
-                break;
-        }
-        accesoriosConsola = opcionesAccesorios();
-    }
+        <div class="card text-dark" style="width: 18rem;">
+            <img class="card-img-top" src="${p.img}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${p.nombre}</h5>
+                <p>$${p.precio}</p>
+                <p>Cantidad: ${p.cantidad}</p>
+                <button class="btn btn-danger">Eliminar</button>
+            </div>
+        </div>
+        `
+
+        producto.querySelector('button').addEventListener('click', ()=>{
+        
+            eliminarProducto(index)
+        })
+
+        carritoHTML.appendChild(producto);
+    })
 }
 
-function seleccionarCarrito(){
-    let precioFinal = carritoTotal.reduce(
-        (acumulador, elemento) => acumulador + elemento, 0
-    );
-    function opcionesCarrito(){
-        let carrito = prompt(
-            `Su carrito tiene acumulado un total de $${precioFinal}
-            \nElija una opción.
-            \n1- Comprar.
-            \n2- Eliminar Carrito.
-            \nPresioná X para salir.`
-        );
-    return carrito;
-    }
-    carrito = opcionesCarrito();
-    while (carrito != "X" && carrito != "x"){
-        switch(carrito){
-            case '1':
-                if (carritoItems == ''){
-                    alert('El carrito está vacío');
-                    break;
-                }else{
-                    comprar = alert('El total será de $' + precioFinal)
-                    carritoItems.length = 0;
-                    carritoTotal.splice(0, carritoTotal.length);
-                    precioFinal = 0;
-                    break;
-                };
-            case '2':
-                if (carritoItems != "") {
-                    carritoItems.length = 0;
-                    carritoTotal.splice(0, carritoTotal.length);
-                    precioFinal = 0;
-                    alert("Carrito vaciado");
-                    break;
-                }else{
-                    alert('El carrito ya está vacío')
-                };
-                break;
-            default:
-                alert('Opción inválida');
-                break;
-        }
-        carrito = opcionesCarrito();
+
+function eliminarProducto(index){
+
+    carrito[index].cantidad--;
+    alert(`La cantidad del producto ${carrito[index].nombre} disminuyo`);
+
+    if(carrito[index].cantidad === 0){
+
+        carrito.splice(index,1);
+        alert('El producto fue eliminado del carrito');
     }
 
+    renderizarCarrito();
+    calcularTotal()
 }
 
-function opcionesMenu(){
-    let opcionesMenu = prompt(
-        `¿Qué desea comprar?
-        \n1- Consolas PS5.
-        \n2- Accesorios PS5.
-        \n3- Carrito.
-        \nPresione X para salir.`
-    );
-    return opcionesMenu;
-}
+function calcularTotal(){
 
-let opcion = opcionesMenu();
-while (opcion != "X" && opcion != "x") {
-    switch(opcion){
-        case "1":
-            seleccionarConsola();
-            break;
-        case "2": 
-            seleccionarAccesorios();
-            break;
-        case "3":
-            seleccionarCarrito();
-            break;
-        default:
-            alert('Opción inválida');
-            break;  
-    }
-    opcion = opcionesMenu();
+    let total = 0;
+
+    carrito.forEach((p)=>{
+    
+        total += p.precio * p.cantidad;
+    })
+
+    console.log(total);
+
+    const t = document.getElementById('total');
+
+    t.innerHTML = `<h5>$${total}</h5>`
+
 }
