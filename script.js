130d39e101e064d16f9e5ea4e8f4e49ac895a1a8
ezearagon <magiceze@gmail.com>
2022-10-13 20:55:03 -0300

    Agregado Fetch

--- script.js
+++ script.js
@@ -1,60 +1,22 @@
-const productos = [
-    {
-        id: 1,
-        categoria: "Combos PS5",
-        nombre: "PlayStation 5 Standart 825gb + 2 Joysticks + Juego físico de regalo",
-        precio: 300000,
-        img: "./assets/img/ps5-2joy-1juego.jpeg",
-        cantidad: 1
-    },
-    {
-        id: 2,
-        categoria: "Combos PS5",
-        nombre: "PlayStation 5 Digital 825gb + Joystick",
-        precio: 280000,
-        img: "./assets/img/ps5-1joy.jpg",
-        cantidad: 1
-    },
-    {
-        id: 3,
-        categoria: "Combos PS5",
-        nombre: "PlayStation 5 Digital 825gb",
-        precio: 270000,
-        img: "./assets/img/ps5-digital.JPG",
-        cantidad: 1
-    },
-    {
-        id: 4,
-        categoria: "Accesorios",
-        nombre: "Joystick DualSense (color a elección)",
-        precio: 15000,
-        img: "./assets/img/1joy.JPG",
-        cantidad: 1
-    },
-    {
-        id: 5,
-        categoria: "Accesorios",
-        nombre: "2 Joystick DualSense (color a elección)",
-        precio: 28000,
-        img: "./assets/img/2joy.png",
-        cantidad: 1
-    },
-    {
-        id: 6,
-        categoria: "Accesorios",
-        nombre: "Auriculares PS5 Sony Pulse 3D",
-        precio: 60000,
-        img: "./assets/img/auris.jpeg",
-        cantidad: 1
+const cargarJson = async() => {
+    try {
+        const response = await fetch("./stock.json");
+        const json = await response.json();
+
+        return json;
+    } catch (error) {
+        console.log('Error',error);
     }
-];
+    cargarJson();
+};
+
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-function renderizarProductos(){
+ async function renderizarProductos(){
     
     const tienda = document.getElementById('tienda');  
-
+    const productos = await cargarJson();
     productos.forEach((p) => {
         let producto = document.createElement('div');
         producto.classList.add('col-12');
@@ -88,8 +50,10 @@ function renderizarProductos(){
 
 renderizarProductos();
 
-function agregarProductosAlCarrito(id){
+async function agregarProductosAlCarrito(id){
     
+    const productos = await cargarJson();
+
     let producto = productos.find(producto => producto.id === id);
 
     let productoEnCarrito = carrito.find(producto => producto.id === id);
@@ -121,8 +85,8 @@ function agregarProductosAlCarrito(id){
     calcularTotal();
 };
 
-function renderizarCarrito(){
-
+async function renderizarCarrito(){
+    
     const d = document;
     let carritoHTML = d.querySelector('#carrito');
 
@@ -191,6 +155,7 @@ function eliminarProducto(index){
             }
         }
         renderizarCarrito();
+        calcularTotal();
     })
     renderizarCarrito();
     calcularTotal()
